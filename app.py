62dc117535c8b66e68cc45343aee53c9f3da3205
Anna <anna.albrecht91@gmail.com>
2021-06-20 21:36:18 +0200

    added entropy check and media queries

--- app.py
+++ app.py
@@ -2,20 +2,22 @@ from flask import Flask, request, render_template
 from password_generator import Password_Generator
 import string
 
+# the flask app
 app = Flask(__name__)
 generator = Password_Generator()
 chars = string.ascii_letters + string.digits
 chars_special = chars + string.punctuation
 
-current_password = "**********"
+current_password = generator.generate_password(15, chars_special)
+first_load = True
 
-@app.route("/", methods=["POST"])
+#index page
+@app.route("/", methods=["GET","POST"])
 def index():
 
     return render_template("index.html", password=current_password)
 
-     
-
+#generate password        
 @app.route("/generate", methods=["POST"])
 def generate():
 
@@ -33,6 +35,15 @@ def generate():
             
     return current_password
 
+#check entropy
+@app.route("/check", methods=["POST"])
+def check_entropy():
+    password = request.form["pass-entropy"]
+    entropy = generator.calc_entropy(password)
+    print(entropy)
+    entropy = round(entropy)
+
+    return str(entropy)
 
 if __name__ == "__main__":
-    app.run(port=1337, debug=True)
+    app.run(host='localhost', port=8080, debug=True)
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,18 +1,33 @@
+//load the buttons, send the requests when clicked
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('button').forEach(button => {
-        button.onclick = () => {
-            data = new FormData(document.getElementById("generate_form"));
-            const request = new XMLHttpRequest();
-            request.open('POST', `/${button.id}`);
-            request.onload = () => {
-                const response = request.responseText;
-                document.getElementById('password').innerHTML = response;
-            }; 
-            request.send(data);
-        };
-    });
+
+    gen_button = document.getElementById("generate-btn")
+    gen_button.onclick = () => {
+        data = new FormData(document.getElementById("generate_form"));
+        const request = new XMLHttpRequest();
+        request.open('POST', `/generate`);
+        request.onload = () => {
+            const response = request.responseText;
+            document.getElementById('password').innerHTML = response;
+        }; 
+        request.send(data);
+    };
+
+    entropy_button = document.getElementById("entropy-btn")
+    entropy_button.onclick = () => {
+        data = new FormData(document.getElementById("entropy-form"));
+        const request = new XMLHttpRequest();
+        request.open('POST', `/check`);
+        request.onload = () => {
+            const response = request.responseText;
+            document.getElementById('entropy-result').innerHTML = response;
+        }; 
+        request.send(data);
+    };
+ 
 });
 
+//change the slider value
 let change_slider = () => {
     var slider = document.getElementById("length-range");
     var output = document.getElementById("len-slider");
